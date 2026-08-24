9b64172f0fb852be067a8eaab465977359f16e15
Derp <78517263+DeRp-MaSte5@users.noreply.github.com>
2026-08-24 16:13:47 -0400

    Add files via upload

---
+++ dots/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --colour=always --hyperlink --grid --icons=always --all --group-directories-first --git-ignore' --description 'alias ls eza --colour=always --hyperlink --grid --icons=always --all --group-directories-first --git-ignore'
+  eza --colour=always --hyperlink --grid --icons=always --all --group-directories-first --git-ignore $argv
+        
+end
