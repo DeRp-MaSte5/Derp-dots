823a21ebdcc77afc05b7f4d6c5377f24b339ac4d
Derp <78517263+DeRp-MaSte5@users.noreply.github.com>
2026-09-14 22:23:06 -0400

    Update hyprland.lua to latest
    events, cursor theme, windowrules, config, animations

--- dots/config/hypr/hyprland.lua
+++ dots/config/hypr/hyprland.lua
@@ -1,13 +1,18 @@
 -- MULTI CONFIG
 require("derp/monitors")
 require("derp/binds")
+require("derp/events")
+require("derp/cursor_theme")
+require("derp/windowrules")
+require("derp/config")
+require("derp/animations"
+)
 -- Variables
-local ide = "subl"
+local ide = "vscodium"
 -- Sublime text, uses subl as its command
 
 local terminal = "kitty"
 local file_explorer = "nemo"
 local launcher = "wofi --show drun"
-
 
 
